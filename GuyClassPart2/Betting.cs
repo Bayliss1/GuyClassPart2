38308f418b519fd8356cb92eba204ddc43ca63cf
Owen <owen.bayliss@sky.com>
2022-06-14 21:37:28 +0100

    Add project files.

---
+++ GuyClassPart2/Betting.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuyClassPart2
+{
+    public class Betting
+    {
+        Random random = new Random();
+        static double odds = 0.75;
+
+        public static void WelcomePlayer()
+        {
+            Console.WriteLine("Welcome to the Casino, the odds are " + Betting.odds);
+        }
+
+        public static int DoubleOrNothing(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(amount + " isn't a valid amount at this casino.");
+                return 0;
+            }
+            if (amount < Cash)
+            {
+                Console.WriteLine("You cannot afford to bet this much.");
+                return 0;
+            }
+            else if (amount >= Cash)
+            {
+                double roll = random.NextDouble();
+                if (roll <= odds)
+                {
+
+                }
+            }
+        }
+        
+    }
+}
---
+++ GuyClassPart2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GuyClassPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guy player = new Guy() { Name = "The player", Cash = 100 };
+
+            Betting.WelcomePlayer();
+            player.WriteMyInfo();
+
+            Console.WriteLine("How much would you like to bet? ");
+            string howMuch = Console.ReadLine();
+            if (int.TryParse(howMuch, out int amount))
+            {
+                int pot = 2 * amount;
+                int Winnings = Betting.DoubleOrNothing(amount);
+            }
+
+        }
+    }
+}
